710d0ab66a5555f5f1b5b34d05a6f2520ddbff2b
AayushMohan <aayushmohan1702@gmail.com>
2022-10-01 22:33:52 +0530

    Added: User Logo

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Bars3Icon, UserCircleIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <div>
-      <h1>Abell</h1>
+    <div className="flex justify-between p-2 cursor-pointer md:hidden">
+      {/* Hamburger Icon */}
+      <div className="flex space-x-4">
+        <Bars3Icon height={24} width={24} />
+        <h1>Abell</h1>
+      </div>
+
+      {/* User Details */}
+      <div>
+        <UserCircleIcon height={24} width={24} />
+      </div>
     </div>
   );
 };
