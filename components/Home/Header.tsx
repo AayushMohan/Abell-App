7edb55b40fb5a59db87bfa0d8c3bf5623fc9088f
AayushMohan <aayushmohan1702@gmail.com>
2022-10-02 23:29:55 +0530

    Added: Desktop View for Header

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -10,18 +10,33 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <div className="flex justify-between p-2 cursor-pointer md:hidden">
-      {/* Hamburger Icon */}
-      <div className="flex space-x-4">
-        <Bars3Icon height={24} width={24} />
-        <h1>Abell</h1>
+    <div>
+      {/* Mobile View Header */}
+      <div className="flex justify-between p-2 cursor-pointer md:hidden border-b-2 m-2">
+        {/* Hamburger Icon */}
+        <div className="flex space-x-4">
+          <Bars3Icon height={24} width={24} />
+          <h1>Abell</h1>
+        </div>
+
+        {/* User Details */}
+        <div className="flex space-x-2 items-center">
+          <MagnifyingGlassIcon height={24} width={24} />
+          <HeartIcon height={24} width={24} />
+          <ShoppingBagIcon height={24} width={24} />
+        </div>
       </div>
 
-      {/* User Details */}
-      <div className="flex space-x-2 items-center">
-        <MagnifyingGlassIcon height={24} width={24} />
-        <HeartIcon height={24} width={24} />
-        <ShoppingBagIcon height={24} width={24} />
+      {/* TODO: Build Header For Desktop Site */}
+
+      {/* Desktop View Header */}
+      <div className="flex items-center space-x-10 p-2 m-2">
+        <h1 className="text-2xl ">Abell</h1>
+        <div className="flex items-center space-x-10 uppercase">
+          <h2>Men</h2>
+          <h2>Women</h2>
+          <h2>Kids</h2>
+        </div>
       </div>
     </div>
   );
